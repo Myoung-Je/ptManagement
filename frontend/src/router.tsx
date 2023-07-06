d52b1b37eb6ad5b8fc4479bdd13d0516623c5b60
Myoung-Je <mjsj1218@naver.com>
2023-07-06 23:35:10 +0900

    main index추가

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -3,3 +3,13 @@ import * as React from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 
 import { reactLazy } from '@/utils/lazy/reactLazy';
+
+const Router = (props: {open: boolean}) => {
+    return (
+        <React.Suspense>
+            <Routes>
+                <Route index element={<Navigate to='/home' />}/>
+            </Routes>
+        </React.Suspense>
+    )
+}
