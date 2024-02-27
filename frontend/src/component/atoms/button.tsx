4e2a7f889cd4a0576b68a6adf67044b2a53428f9
Myoung-Je <mjsj1218@naver.com>
2024-02-27 20:30:52 +0900

    nav바 생성

--- frontend/src/component/atoms/button.tsx
+++ frontend/src/component/atoms/button.tsx
@@ -1,3 +1,40 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const Button = styled.button``;
+export interface Iprops {
+    width?: number | string;
+    height?: number | string;
+}
+
+const Button = styled.button<Iprops>`
+    border: none;
+    outline: none;
+    padding: 0 12px;
+
+    ${(props) => {
+        switch (typeof props.width) {
+            case 'number':
+                return css`
+                    width: ${props.width}px;
+                `;
+
+            case 'string':
+                return css`
+                    width: ${props.width};
+                `;
+        }
+    }}
+
+    ${(props) => {
+        switch (typeof props.height) {
+            case 'number':
+                return css`
+                    height: ${props.height}px;
+                `;
+
+            case 'string':
+                return css`
+                    height: ${props.height};
+                `;
+        }
+    }}
+`;
