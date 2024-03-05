02c317827cc1cf8204af5eee0408f14473197e2a
Myoung-Je <mjsj1218@naver.com>
2024-03-06 00:05:50 +0900

    topnav 제작

--- frontend/src/component/organisms/nav/topNav.tsx
+++ frontend/src/component/organisms/nav/topNav.tsx
@@ -1,7 +1,38 @@
 import * as React from 'react';
+import styled, { css } from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const TopNav = () => {
-    return <div></div>;
+    return (
+        <Wrapper>
+            <Container>
+                <Title></Title>
+            </Container>
+        </Wrapper>
+    );
 };
 
 export default TopNav;
+
+const Wrapper = styled.div``;
+
+const Container = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 80px;
+
+    padding: 0 40px;
+    margin: 0 auto;
+    height: 60px;
+`;
+
+const Title = styled.div``;
+
+const Menu = {
+    Container: styled.div``,
+
+    List: styled.div``,
+
+    ListItem: styled.li``,
+};
