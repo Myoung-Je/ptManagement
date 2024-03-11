a7726bc729684d8f74795557832426b8f24c97a1
Myoung-Je <mjsj1218@naver.com>
2024-03-11 23:35:03 +0900

    globalstyle 세팅

---
+++ frontend/src/styles/globalStyle.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+    ${reset}
+
+    * {
+        margin: 0;
+        padding: 0;
+    }
+`;
