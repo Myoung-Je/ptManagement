e7f00a61355c4019e346524076e81d8056b50d8a
Myoung-Je <mjsj1218@naver.com>
2024-04-10 20:34:58 +0900

    input component 생성

--- frontend/src/component/molecules/burgerMenu.tsx
+++ frontend/src/component/molecules/burgerMenu.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import styled, { css } from 'styled-components';
 
 interface BugerMenuProps {
     isOpen: boolean;
@@ -7,14 +8,29 @@ interface BugerMenuProps {
 
 const BurgerMenu = (props: BugerMenuProps) => {
     return (
-        <div className="burger-menu" onClick={props.onClick}>
+        <Wrapper onClick={props.onClick}>
             <div className={props.isOpen ? 'burger-menu-icon open' : 'burger-menu-icon'}>
                 <div className="bar1"></div>
                 <div className="bar2"></div>
                 <div className="bar3"></div>
             </div>
-        </div>
+        </Wrapper>
     );
 };
 
 export default BurgerMenu;
+
+const Wrapper = styled.div`
+    width: 30px;
+    height: 20px;
+    position: relative;
+    display: inline-block;
+`;
+
+const Bar = styled.div`
+    width: 100%;
+    height: 3px;
+    background-color: #333;
+    margin: 6px 0;
+    transition: 0.4s;
+`;
